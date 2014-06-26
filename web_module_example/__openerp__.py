72935a1e983c029f71250292817003f36c7448b0
glopzvega <mcgalv@gmail.com>
2014-06-26 13:27:36 -0500

    Initial Commit Adding Modules

---
+++ web_module_example/__openerp__.py
@@ -0,0 +1,10 @@
+{
+    'name': "Web Example",
+    'description': "Basic example of a (future) web module",
+    'category': 'Hidden',
+    'depends': ['web'],
+    'data': ['web_example.xml'],
+    'css': ['static/src/css/web_module_example.css'],
+    'js': ['static/src/js/test.js'],
+    'qweb': ['static/src/xml/web_module_example.xml'],    
+}
